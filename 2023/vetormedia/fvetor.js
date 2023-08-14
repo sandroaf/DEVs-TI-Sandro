42220d3716ff2e0295a380b4098c263ca1c44539
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2023-08-14 15:22:15 -0300

    Exercício 2 Media de valores em vetor.

---
+++ 2023/vetormedia/fvetor.js
@@ -0,0 +1,23 @@
+var vetornum = [];
+
+function fVetorIncluir(numero) {
+    vetornum.push(numero); //função array.push() adiciona elementos no fim do array.
+    return "Incluído com sucesso - Nro de valores: "+vetornum.length+"\nSendo:"+vetornum.toString(); 
+}
+
+
+/* fVetorMedia - Calcula a média de valores de um vetor numérico. atualiza a lista dos valores e a média no elemento <HTML> passado por parametro
+@param String idResultado do html para mostrar resultado*/
+function fVetorMedia(idResultado) {
+    let media = 0;
+    let SomaValores = 0;
+    const vidResultado = document.querySelector("#"+idResultado);
+    vidResultado.innerHTML = "<ol>"; 
+    for (i = 0;i < vetornum.length; i++) {
+        SomaValores += vetornum[i];       
+        vidResultado.innerHTML += "<li>"+vetornum[i]+"</li>" 
+    }
+    vidResultado.innerHTML += "</ol>"
+    media = SomaValores / vetornum.length;
+    vidResultado.innerHTML += "<br>Média: "+media;
+}
